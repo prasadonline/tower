800866d1c3677d8f7106f082b6da59c627f70bb3
Prasad D. Warnakulasuriya <me@prasadonline.com>
2024-02-18 05:25:36 +0530

    Registering main menu

--- functions.php
+++ functions.php
@@ -9,3 +9,15 @@ function reg_scripts() {
     wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array(), true );
 }
 add_action('wp_enqueue_scripts', 'reg_scripts');
+
+/*
+ * Registering Menu
+ */
+if ( function_exists( 'register_nav_menus' ) ) {
+    register_nav_menus(
+        array(
+            'main-menu'   => 'Main Navigation Menu'
+        )
+    );
+}
+
