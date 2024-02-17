a38816f31c9e0910d99ab19c4f24ba3fbaecf1da
Prasad D. Warnakulasuriya <me@prasadonline.com>
2024-02-18 05:23:26 +0530

    Adding bootstrap support to Tower theme

--- functions.php
+++ functions.php
@@ -1,1 +1,11 @@
 <?php
+
+/*
+ * Adding bootstrap CSS/JS
+ */
+function reg_scripts() {
+    wp_enqueue_style( 'bootstrapstyle', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrapthemestyle', get_template_directory_uri() . '/css/bootstrap-theme.min.css' );
+    wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array(), true );
+}
+add_action('wp_enqueue_scripts', 'reg_scripts');
