3d0ccb7d21a09a6b9d0d8f460a26bff830284af0
Prasad D. Warnakulasuriya <me@prasadonline.com>
2024-02-18 06:34:48 +0530

    Insurance policy claim custom post type created with custom fields

--- functions.php
+++ functions.php
@@ -167,3 +167,142 @@ add_action( 'save_post_insurance_policy', 'insurance_policy_save_meta_box_data',
 
 
 // End of Insurance Policy Custom meta
+// Start of Insurance Policy Claim Custom meta
+
+
+/**
+ * Register Custom Post Type -Insurance Policy Claim
+ * @return void
+ */
+function insurance_claim() {
+
+    $labels = array(
+        'name'                  => _x( 'Insurance Policy Claims', 'Post Type General Name', 'tower' ),
+        'singular_name'         => _x( 'Insurance Policy Claim', 'Post Type Singular Name', 'tower' ),
+        'menu_name'             => __( 'Insurance Policy Claim Types', 'tower' ),
+        'name_admin_bar'        => __( 'Insurance Policy Claim Type', 'tower' ),
+        'archives'              => __( 'Insurance Policy Claim Archives', 'tower' ),
+        'attributes'            => __( 'Insurance Policy Claim Attributes', 'tower' ),
+        'parent_item_colon'     => __( 'Parent Item:', 'tower' ),
+        'all_items'             => __( 'All Items', 'tower' ),
+        'add_new_item'          => __( 'Add New Insurance Policy', 'tower' ),
+        'add_new'               => __( 'Add New', 'tower' ),
+        'new_item'              => __( 'New Insurance Policy Claim', 'tower' ),
+        'edit_item'             => __( 'Edit Insurance Policy Claim', 'tower' ),
+        'update_item'           => __( 'Update Insurance Policy Claim', 'tower' ),
+        'view_item'             => __( 'View Insurance Policy Claim', 'tower' ),
+        'view_items'            => __( 'View Insurance Policy Claim', 'tower' ),
+        'search_items'          => __( 'Search Insurance Policy Claim', 'tower' ),
+        'not_found'             => __( 'Not found', 'tower' ),
+        'not_found_in_trash'    => __( 'Not found in Trash', 'tower' ),
+        'featured_image'        => __( 'Featured Image', 'tower' ),
+        'set_featured_image'    => __( 'Set featured image', 'tower' ),
+        'remove_featured_image' => __( 'Remove featured image', 'tower' ),
+        'use_featured_image'    => __( 'Use as featured image', 'tower' ),
+        'insert_into_item'      => __( 'Insert into Insurance Policy Claim', 'tower' ),
+        'uploaded_to_this_item' => __( 'Uploaded to this Insurance Policy Claim', 'tower' ),
+        'items_list'            => __( 'Items list', 'tower' ),
+        'items_list_navigation' => __( 'Items list navigation', 'tower' ),
+        'filter_items_list'     => __( 'Filter items list', 'tower' ),
+    );
+    $rewrite = array(
+        'slug'                  => 'insurance_claim',
+        'with_front'            => true,
+        'pages'                 => true,
+        'feeds'                 => true,
+    );
+    $args = array(
+        'label'                 => __( 'Insurance Policy Claim', 'tower' ),
+        'description'           => __( 'Insurance Policy Claim Description', 'tower' ),
+        'labels'                => $labels,
+        'supports'              => array( 'title', 'custom-fields' ),
+        'taxonomies'            => array( 'category', 'post_tag' ),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 5,
+        'menu_icon'             => 'dashicons-media-text',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'rewrite'               => $rewrite,
+        'capability_type'       => 'post',
+        'show_in_rest'          => true,
+    );
+    register_post_type( 'insurance_claim', $args );
+
+}
+add_action( 'init', 'insurance_claim', 0 );
+
+
+/**
+ * Add meta box
+ * @param $post
+ * @return void
+ */
+function insurance_claim_meta_boxes( $post ){
+    add_meta_box( 'insurance_claim_meta_box', __( 'Insurance Policy Claim Details', 'tower' ), 'insurance_claim_build_meta_box', 'insurance_claim', 'normal', 'high' );
+}
+add_action( 'add_meta_boxes_insurance_claim', 'insurance_claim_meta_boxes' );
+
+/**
+ * Meta box UI
+ * @param $post
+ * @return void
+ */
+function insurance_claim_build_meta_box( $post ){
+
+    // make sure the form request comes from WordPress
+    wp_nonce_field( basename( __FILE__ ), 'insurance_claim_meta_box_nonce' );
+
+    $insurance_claim_id = get_post_meta( $post->ID, '_insurance_claim_id', true);
+    $insurance_claim_name = get_post_meta( $post->ID, '_insurance_claim_name', true);
+    $insurance_claim_email = get_post_meta( $post->ID, '_insurance_claim_email', true);
+    ?>
+
+    <div class='inside'>
+        <p>
+            <label>Policy ID<sup style="color: red">*</sup></label>
+            <input type="text" name="insurance_claim_id" value="<?php echo $insurance_claim_id; ?>" required/>
+        </p>
+        <p>
+            <label>Name<sup style="color: red">*</sup></label>
+            <input type="text" name="insurance_claim_name" value="<?php echo $insurance_claim_name; ?>" required/>
+        </p>
+        <p>
+            <label>Email<sup style="color: red">*</sup></label>
+            <input type="email" name="insurance_claim_email" value="<?php echo $insurance_claim_email; ?>" required/>
+        </p>
+    </div>
+<?php }
+
+/**
+ * Save meta box data
+ * @param $post_id
+ * @return void
+ */
+function insurance_claim_save_meta_box_data( $post_id ){
+    if ( !isset( $_POST['insurance_claim_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['insurance_claim_meta_box_nonce'], basename( __FILE__ ) ) ){
+        return;
+    }
+    if ( isset( $_REQUEST['insurance_claim_id'] ) ) {
+        update_post_meta( $post_id, '_insurance_claim_id', sanitize_text_field( $_POST['insurance_claim_id'] ) );
+    }
+
+    if ( isset( $_REQUEST['insurance_claim_name'] ) ) {
+        update_post_meta( $post_id, '_insurance_claim_name', sanitize_text_field( $_POST['insurance_claim_name'] ) );
+    }
+    if ( isset( $_REQUEST['insurance_claim_email'] ) ) {
+        update_post_meta( $post_id, '_insurance_claim_email', sanitize_text_field( $_POST['insurance_claim_email'] ) );
+    }
+
+}
+add_action( 'save_post_insurance_claim', 'insurance_claim_save_meta_box_data', 10, 2 );
+
+
+
+// End of Insurance Policy Claim Custom meta
